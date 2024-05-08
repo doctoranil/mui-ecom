be1c3e7547cd164a5b87220e2a033a3028ad34bf
anil <anil15124@gmail.com>
2024-05-09 01:33:46 +0530

    update package and components

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 interface MyComponentProps {
-  // Define props here components
+  // Define props here mycomponent
 }
 
 const MyComponent: React.FC<MyComponentProps> = () => {
   return (
     <div>
       {/* Your component JSX here */}
-      Hello
+      Hello Package for testing
     </div>
   );
 };
